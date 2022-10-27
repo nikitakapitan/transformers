d109a0e0c8b01788ef09a3704d7c712fd38fdb90
nikitakapitan <kapchenko.nikita@gmail.com>
2022-10-27 12:38:05 +0200

    first push from VSCode

--- src/transformers/EncoderDecoder.py
+++ src/transformers/EncoderDecoder.py
@@ -8,23 +8,24 @@ src_emb - embeddings of source input
 tgt_emb - embeddings of target input
 generator -
 """
-import toch as nn
+import torch as nn
 
 class EncoderDecoder(nn.Module):
-	def __init__(self, encoder, decoder, src_emb, tgt_emb, generator ):
-		self.encoder = encoder
+
+    def __init__(self, encoder, decoder, src_emb, tgt_emb, generator):
+        self.encoder = encoder
         self.decoder = decoder
         self.src_emb = src_emb
         self.tgt_emb = tgt_emb
         self.generator = generator
-	
-	def forward(self, src, tgt, src_mask, tgt_mask  ):
-		encoding = self.encode(src, src_mask)
-        decoging = self.decode(encoding, src_mask, tgt, tgt_mask)
+
+    def forward(self, src, tgt, src_mask, tgt_mask):
+        encoding = self.encode(src, src_mask)
+        decoding = self.decode(encoding, src_mask, tgt, tgt_mask)
         return decoding
         
     def encode(self, src, src_mask):
-        return self.encoder(self.src_emb(src), src_mask))
+        return self.encoder(self.src_emb(src), src_mask)
      
     def decode(self, memory, src_mask, tgt, tgt_mask):
         return self.decoder(self.tgt_emb(tgt), memory, src_mask, tgt_mask)
