dd46b6075120d465de3efcb34ebe2fb4147db515
nikitakapitan <kapchenko.nikita@gmail.com>
2022-10-29 19:27:20 +0200

    +positional FeedFwd & Embeddings

--- src/transformers/mask.py
+++
@@ -1,8 +0,0 @@
-
-import torch
-
-def following_mask(size):
-    "Mask out subsequent positions."
-    attn_shape = (1, size, size)
-    following_mask = torch.tril(torch.ones(attn_shape)).type(torch.uint8)
-    return following_mask
