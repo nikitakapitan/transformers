3c8b1fc1c33228e15f524454dce1e0f209531e14
nikitakapitan <kapchenko.nikita@gmail.com>
2022-10-31 18:53:26 +0100

    DEMO.ipynb

--- src/transformers/Embeddings.py
+++ src/transformers/Embeddings.py
@@ -4,10 +4,10 @@ import math
 import torch.nn as nn
 
 class Embeddings(nn.Module):
-    def __init__(self, d_model, vocab) -> None:
+    def __init__(self, vocab, d_model) -> None:
         super().__init__()
-        self.lut = nn.Embedding(vocab, d_model)
+        self.emb = nn.Embedding(vocab, d_model)
         self.d_model = d_model
 
     def forward(self, x):
-        return self.lut(x) * math.sqrt(self.d_model)
+        return self.emb(x) * math.sqrt(self.d_model)
--- src/transformers/PositionalEncoding.py
+++ src/transformers/PositionalEncoding.py
@@ -10,15 +10,15 @@ class PositionalEncoding(nn.Module):
         self.dropout = nn.Dropout(p=dropout)
 
         pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len).unsqueeze(1)
+        position = torch.arange(0, max_len).unsqueeze(1) # (max_len, 1)
         div_term = torch.exp(
             torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model)
         )
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0)
+        pe = pe.unsqueeze(0) # (1, max_len, d_model)
         self.register_buffer("pe", pe) # not learnable parameter
 
     def forward(self, x):
-        x = x + self.pe[:,:, x.size(1)].requires_grad_(False)
+        x = x + self.pe[:,x.size(1), :].requires_grad_(False)
         return self.dropout(x)
